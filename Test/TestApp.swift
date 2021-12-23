def33baf7e3e7e67b7b335482cf90cace30444ff
niemczus <87914966+niemczus@users.noreply.github.com>
2021-12-23 12:55:26 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Kamil on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
